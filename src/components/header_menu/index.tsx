6a471eb218eb59dc658d81be06076859b4cdb155
Davyd Kewen <dcsistemas3@gmail.com>
2021-09-20 12:50:32 -0300

    .

--- src/components/header_menu/index.tsx
+++ src/components/header_menu/index.tsx
@@ -5,6 +5,12 @@ const HeaderMenu: React.FC = () => {
     return (
         <div className="container-menu" >
             <h1 className="title-menu" >Faxina Express</h1>
+            <ul>
+                <li>Inicio</li>
+                <li>Sobre Nós</li>
+                <li>Blog</li>
+                <li>Contato</li>
+            </ul>
         </div>
     )
 }
